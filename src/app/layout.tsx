b3f0fe570bb8d29564ebe4b63e2a75c0330783c3
am-ramona <ramona.abimoussa@gmail.com>
2024-08-13 15:58:43 +0300

    Added Fonts as per the Brand Bok: IBM Plex Mono and IBM Plex Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,21 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from 'next'
+import { IBM_Plex_Mono, IBM_Plex_Sans } from 'next/font/google'
+import { NavLinks } from '@/app/ui/nav-links'
+import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = IBM_Plex_Sans({ 
+  weight: ['700'], // Specify the weight you need for titles (bold)  
+  subsets: ['latin'],  
+  display: 'swap',
+ });
+
+// Create the font config specifying weights and subsets if required  
+const ibmPlexMono = IBM_Plex_Mono({  
+  weight: ['400', '500', '600'], // Specify desired weights  
+  style: ['normal', 'italic'],
+  subsets: ['latin'], // Specify subsets if needed  
+  display: 'swap',   // Optional: How to display the font  
+}); 
 
 export const metadata: Metadata = {
   title: 'Crypto Custodian',
@@ -17,7 +30,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <NavLinks />
+      <main>{children}</main>
+      </body>
     </html>
   );
 }
---
+++ src/app/ui/nav-links.tsx
@@ -0,0 +1,51 @@
+'use client'
+ 
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+ 
+export function NavLinks() {
+  const pathname = usePathname()
+ 
+  return (
+    <nav>
+      <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">
+        Dashboard
+      </Link>
+ 
+      <Link
+        className={`link ${pathname === '/about' ? 'active' : ''}`}
+        href="/assetOverview"
+      >
+        Assets Overview
+      </Link>
+
+      <Link
+        className={`link ${pathname === '/about' ? 'active' : ''}`}
+        href="/positionsAndHoldings"
+      >
+        Positions and Holdings
+      </Link>
+
+      <Link
+        className={`link ${pathname === '/about' ? 'active' : ''}`}
+        href="/aprAndPerformanceMetrics"
+      >
+        APR and Performance Metrics
+      </Link>
+
+      <Link
+        className={`link ${pathname === '/about' ? 'active' : ''}`}
+        href="/clientManagement"
+      >
+        Client Management
+      </Link>
+
+      <Link
+        className={`link ${pathname === '/about' ? 'active' : ''}`}
+        href="/transactionAndCompliance"
+      >
+        Transaction and Compliance
+      </Link>
+    </nav>
+  )
+}
